735072650b71ed093b9aef12fd5c2a48ce88dd04
davidd <davo.27@yandex.ru>
2021-01-20 17:54:41 +0400

    stable version

---
+++ db.py
@@ -0,0 +1,40 @@
+import sqlite3
+from datetime import datetime
+from os import environ
+import time
+
+
+# environ['TZ'] = 'Asia/Dubai'
+# time.tzset()
+
+
+class Database:
+    dt = datetime.now().strftime('%d/%m/%Y')
+    database = 'database.db'
+    connection = sqlite3.connect(database, check_same_thread=False)
+    cursor = connection.cursor()
+
+    '''Регистрирует пользователя'''
+    def add_user(self, username, password_hash, time=dt):
+        with self.connection:
+            return self.cursor.connection.execute(
+                'INSERT INTO users (username, hash, datetime) VALUES (?, ?, ?)', (username, password_hash, time)
+            )
+
+    '''Проверяет данные от пользователя'''
+    def check_data(self, login):
+        with self.connection:
+            return self.cursor.connection.execute(
+                'SELECT id, username, hash, datetime FROM users WHERE username = ?', (login,)).fetchone()
+
+    '''Добавляет новую запись'''
+    def add_post(self, login, title, text):
+        with self.connection:
+            return self.cursor.connection.execute(
+                'INSERT INTO posts (username, title, text) VALUES (?, ?, ?)', (login, title, text))
+
+    '''Показывает посты по имени'''
+    def get_posts(self, login):
+        with self.connection:
+            return self.cursor.connection.execute(
+                'SELECT title, text FROM posts WHERE username = ?', (login,)).fetchall()
--- server.py
+++ server.py
@@ -1,15 +1,11 @@
-from flask import Flask, render_template
-from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime as dt
+from flask import Flask, render_template, request, redirect, flash, session, url_for
+from db import Database
+import bcrypt
 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db = SQLAlchemy(app)
-
-
-
+app.config['SESSION_TYPE'] = 'memcached'
+app.config['SECRET_KEY'] = '+dZL@dds^xyzNG.ifgjWO-]8v>Cj)xe1}t@CiEyNU4#)8ZenoaOeN"3+'
 
 
 @app.route('/')
@@ -18,18 +14,88 @@ def index():
 
 
 @app.route('/add')
-def about():
+def add():
     return render_template('add.html')
 
 
-@app.route('/auth')
+@app.route('/profile', methods=['GET', 'POST'])
+def profile():
+    if 'user' in session:
+        data = Database()
+        user_data = data.check_data(session.get('user'))
+        posts = data.get_posts(session.get('user'))
+        if request.method == 'POST':
+            title = request.form['title']
+            text = request.form['text']
+            data.add_post(session.get('user'), title, text)
+            flash('Данные были отправлены!')
+            return redirect(url_for('profile'))
+        else:
+            return render_template('profile.html', user_data=user_data, posts=posts)
+    else:
+        return render_template('404.html', cause='Чтобы просмотреть эту страницу, нужно авторизоваться!')
+
+
+@app.route('/auth', methods=['POST', 'GET'])
 def auth():
-    return render_template('auth.html')
+    if 'user' in session:
+        return redirect(url_for('profile'))
+    if request.method == 'POST':
+        data = Database()
+        login = request.form['login']
+        password = request.form['pass'].encode('UTF-8')
+        user_data = data.check_data(login.lower())
+        if user_data:
+            password_hash = bcrypt.checkpw(password, user_data[2])
+            if password_hash:
+                session['user'] = login
+                return redirect(url_for('profile'))
+            else:
+                flash('Пароль не совпадает!')
+                return render_template('auth.html')
+        else:
+            flash('Не найден аккаунт!')
+            return render_template('auth.html')
+    else:
+        return render_template('auth.html')
 
 
-@app.route('/register')
+@app.route('/register', methods=['POST', 'GET'])
 def register():
-    return render_template('register.html')
+    if 'user' in session:
+        return redirect(url_for('profile'))
+    if request.method == 'POST':
+        data = Database()
+        login = request.form['username']
+        password = request.form['pass'].encode('UTF-8')
+        password2 = request.form['pass2'].encode('UTF-8')
+        name = data.check_data(login.lower())
+        if not name:
+            if len(login) > 3 and len(password) > 4:
+                if password == password2:
+                    password_hash = bcrypt.hashpw(password, bcrypt.gensalt())
+                    data.add_user(login, password_hash)
+                    return redirect(url_for('auth'))
+                else:
+                    flash('Пароли не совпадают!')
+                    return render_template('register.html')
+            else:
+                flash('Логин и пароль должны быть больше 4 символов')
+                return render_template('register.html')
+        else:
+            flash(f'Пользователь с именем {login} уже зарегистрирован')
+            return render_template('register.html')
+    else:
+        return render_template('register.html')
+
+
+@app.route('/logout', methods=['GET'])
+def logout():
+    if 'user' in session:
+        session.pop('user', None)
+        return redirect(url_for('index'))
+    else:
+        return render_template('404.html', cause='Ты не авторизован')
 
 
 if __name__ == '__main__':
